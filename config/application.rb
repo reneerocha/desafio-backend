da6da68a9b0bca80224220d94c8cb9ef716d544c
Renee Rocha <reneerocha.pi@gmail.com>
2022-07-13 16:38:50 -0300

    add translate pt-BR

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module DesafioBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    Time::DATE_FORMATS[:default] = "%d/%m/%Y"
+    Date::DATE_FORMATS[:default] = "%d/%m/%Y"
+    config.i18n.default_locale = :"pt-BR"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
